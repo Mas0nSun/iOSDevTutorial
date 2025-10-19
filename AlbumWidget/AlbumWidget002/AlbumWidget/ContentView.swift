210d87e2ba0cc36e382f1575f0bee3c1c31f4e80
Mason Sun <421680227@qq.com>
2025-10-20 01:40:54 +0800

    更新目录结构

---
+++ AlbumWidget/AlbumWidget002/AlbumWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlbumWidget
+//
+//  Created by Mason Sun on 2025/8/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
